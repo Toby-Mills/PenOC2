9d3aa50dab15d44fcd42fe793f13cde1812f8e36
Toby-Mills <Mills.toby@gmail.com>
2017-06-19 14:53:08 +0200

    Upgraded to Angular ~4.0.0

--- PenOC2/Admin/app/components/result-list/result-list.component.js
+++ PenOC2/Admin/app/components/result-list/result-list.component.js
@@ -8,7 +8,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var result_service_1 = require("../../services/result.service");
 var course_model_1 = require("../../models/course.model");
--- PenOC2/Admin/app/models/result.model.js
+++ PenOC2/Admin/app/models/result.model.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var ResultModel = (function () {
     function ResultModel() {
     }
