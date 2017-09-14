fb62d843c06b1cb838f69ea045c2df065e4554e4
Toby-Mills <Mills.toby@gmail.com>
2017-09-14 15:39:02 +0200

    Migrated SDK Module out of Front End, into its own repo, to be consumed as a SubTree

---
+++ models/club.model.js
@@ -0,0 +1,8 @@
+"use strict";
+var ClubModel = (function () {
+    function ClubModel() {
+    }
+    return ClubModel;
+}());
+exports.ClubModel = ClubModel;
+//# sourceMappingURL=club.model.js.map
---
+++ models/course.model.js
@@ -0,0 +1,8 @@
+"use strict";
+var CourseModel = (function () {
+    function CourseModel() {
+    }
+    return CourseModel;
+}());
+exports.CourseModel = CourseModel;
+//# sourceMappingURL=course.model.js.map
