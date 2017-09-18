3c1d202e86a6a402c15311bee81da770a46b99b6
Toby-Mills <Mills.toby@gmail.com>
2017-09-18 16:59:19 +0200

    fixed import of rxjs

--- services/api.service.ts
+++ services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptionsArgs, Response } from '@angular/http';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ApiService {
